788940309a0bc99281dbe8b7c4ac9a4df0e84f72
Danielle Pabon <daniellepabon@Danielles-MacBook-Air.local>
2018-04-26 16:02:47 -0400

    fixes type in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-var bodyParser = require('body-Parser');
+var bodyParser = require('body-parser');
 
 var PORT = process.env.PORT || 3002;
 
